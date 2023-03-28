31720b1f46c64c48d0adb6db60914ba4e70cc41c
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2023-03-28 00:55:36 -0500

    initial commit

---
+++ cnPtrQueue.cpp
@@ -0,0 +1,30 @@
+#include "cnPtrQueue.h"
+#include <cassert>
+using namespace std;
+
+namespace CS3358_SP2023_A5P2
+{
+    cnPtrQueue::cnPtrQueue(){
+        //constructor
+    }
+
+    bool cnPtrQueue::empty() const {
+        //empty
+    }
+
+    cnPtrQueue::size_type cnPtrQueue::size() const {
+        //size
+    }
+
+    CNode* cnPtrQueue::front() {
+        //front
+    }
+
+    void cnPtrQueue::push(CNode *cnPtr) {
+        //push
+    }
+
+    void cnPtrQueue::pop() {
+        //pop
+    }
+}
