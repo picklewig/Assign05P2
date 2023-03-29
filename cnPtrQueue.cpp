7a1616bb16e20474794b067d5f9155835bbd0ec5
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2023-03-28 21:17:49 -0500

    set definitions for all child node pointer stacks. unsure what testing data is supposed to look like as testing data is unclear

--- cnPtrQueue.cpp
+++ cnPtrQueue.cpp
@@ -4,27 +4,48 @@ using namespace std;
 
 namespace CS3358_SP2023_A5P2
 {
-    cnPtrQueue::cnPtrQueue(){
+    cnPtrQueue::cnPtrQueue(): numItems(0){
         //constructor
     }
 
     bool cnPtrQueue::empty() const {
         //empty
+        return inStack.empty() and outStack.empty();
     }
 
     cnPtrQueue::size_type cnPtrQueue::size() const {
         //size
+        return numItems;
     }
 
     CNode* cnPtrQueue::front() {
         //front
+        assert(!inStack.empty() or !outStack.empty());
+        if(outStack.empty()){
+            while(!inStack.empty()){
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        return outStack.top();
     }
 
-    void cnPtrQueue::push(CNode *cnPtr) {
+    void cnPtrQueue::push(CNode* cnPtr) {
         //push
+        inStack.push(cnPtr);
+        numItems++;
     }
 
     void cnPtrQueue::pop() {
         //pop
+        assert(!inStack.empty() or !outStack.empty());
+        if(outStack.empty()){
+            while(!inStack.empty()){
+                outStack.push(inStack.top());
+                inStack.pop();
+            }
+        }
+        outStack.pop();
+        numItems--;
     }
 }
