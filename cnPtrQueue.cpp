b671bb879f4dfc08c39a65ae6c359f59fa6ae12c
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2023-03-30 19:40:26 -0500

    removed unnecessary comments from cnPterQueue.cpp

--- cnPtrQueue.cpp
+++ cnPtrQueue.cpp
@@ -4,22 +4,17 @@ using namespace std;
 
 namespace CS3358_SP2023_A5P2
 {
-    cnPtrQueue::cnPtrQueue(): numItems(0){
-        //constructor
-    }
+    cnPtrQueue::cnPtrQueue(): numItems(0){}
 
     bool cnPtrQueue::empty() const {
-        //empty
         return inStack.empty() and outStack.empty();
     }
 
     cnPtrQueue::size_type cnPtrQueue::size() const {
-        //size
         return numItems;
     }
 
     CNode* cnPtrQueue::front() {
-        //front
         assert(!inStack.empty() or !outStack.empty());
         if(outStack.empty()){
             while(!inStack.empty()){
@@ -31,13 +26,11 @@ namespace CS3358_SP2023_A5P2
     }
 
     void cnPtrQueue::push(CNode* cnPtr) {
-        //push
         inStack.push(cnPtr);
         numItems++;
     }
 
     void cnPtrQueue::pop() {
-        //pop
         assert(!inStack.empty() or !outStack.empty());
         if(outStack.empty()){
             while(!inStack.empty()){
